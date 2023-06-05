7857fb16459683748912af15cb041a45191c64eb
Juan Lozano <jalozanolopez@gmail.com>
2023-06-05 17:20:46 +0200

    Full CRUD app done

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -51,6 +51,40 @@ router.post("/books/create", (req, res, next) => {
         });
 });
 
+// GET /books/:bookId/edit
+router.get("/books/:bookId/edit", (req, res, next) => {
+    // tests:
+    // res.send("This is working"); 
+    // console.log("This is working");
+
+    const { bookId } = req.params;
+    Book.findById(bookId)
+    .then(bookToEdit => {
+    //   console.log(bookToEdit);
+    res.render('books/book-edit.hbs', { book: bookToEdit });
+    })
+    .catch(error => next(error));
+})
+
+// POST route to actually make updates on a specific book
+router.post('/books/:bookId/edit', (req, res, next) => {
+    const { bookId } = req.params;
+    const { title, description, author, rating } = req.body;
+   
+    Book.findByIdAndUpdate(bookId, { title, description, author, rating }, { new: true })
+      .then(updatedBook => res.redirect(`/books/${updatedBook.id}`)) // go to the details page to see the updates
+      .catch(error => next(error));
+  });
+
+  // POST route to delete a book from the database
+router.post('/books/:bookId/delete', (req, res, next) => {
+    const { bookId } = req.params;
+   
+    Book.findByIdAndDelete(bookId)
+      .then(() => res.redirect('/books'))
+      .catch(error => next(error));
+  });
+
 
 // GET /books/:bookId
 router.get("/books/:bookId", (req, res, next) => {
